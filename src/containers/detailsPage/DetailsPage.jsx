ebef783352fc1b6fa65248a3dd1d8490a0d3c724
Cyber Slug <lasminski@mail.ru>
2022-03-20 17:37:03 +0300

    replace header height with padding

--- src/containers/detailsPage/DetailsPage.jsx
+++ src/containers/detailsPage/DetailsPage.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import {Layout} from "../layout/Layout.jsx";
 
 const DetailsPage = () => (
-  <div>
     <Layout>
       details
     </Layout>
-  </div>
 );
 
 export default DetailsPage;
--- src/containers/layout/Layout.jsx
+++ src/containers/layout/Layout.jsx
@@ -4,7 +4,7 @@ import {Header} from "../header/Header.jsx";
 import './Layout.scss'
 
 export const Layout = ({children}) => (
-  <div>
+  <div className="Layout">
     <Header/>
     {children} 
   </div>
