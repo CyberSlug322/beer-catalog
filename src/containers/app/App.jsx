fc15518e09f766236694bb6db4bdcbc2940ef485
Cyber Slug <lasminski@mail.ru>
2022-03-20 15:07:48 +0300

    add fonts import in _variables.css, remove index.html duplicate

--- src/containers/app/App.jsx
+++ src/containers/app/App.jsx
@@ -4,6 +4,8 @@ import DetailsPage from "../detailsPage/DetailsPage.jsx";
 import FavoritesPage from "../favoritesPage/FavoritesPage.jsx";
 import LandingPage from "../landingPage/LandingPage.jsx";
 
+import '../../styles/_variables.css'
+
 import {
   Route,
   Routes,
@@ -13,7 +15,7 @@ import {
 import "../../styles/_variables.css"
 
 const App = () => (
-  <div className="app">
+  <div className="app font-link">
     <ul>
       <li><Link to="/landing">landing</Link></li>
       <li><Link to="/favorites">favorites</Link></li>
