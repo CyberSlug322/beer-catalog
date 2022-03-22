e097e1bf0f052d859897be62ffdbbb60fabbcf4c
Cyber Slug <lasminski@mail.ru>
2022-03-22 20:59:59 +0300

    add paddings to side menu

--- src/containers/sideMenu/SideMenu.jsx
+++ src/containers/sideMenu/SideMenu.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const SideMenu = () => (
   <div className="side-menu header__side-menu">
     <div className="side-menu__header">
-        head
+        Beer Catalog
     </div>
     <ul className="side-menu__nav-bar">
       <li><Link to="/landing">landing</Link></li>
