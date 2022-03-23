a9a5f2747a778ce9184399caa9583cfadc3ed6a5
CyberSlug322 <58426879+CyberSlug322@users.noreply.github.com>
2022-03-23 22:54:35 +0300

    Feature/fm 96 beer card markup (#10)
    * add empty components with hardcoded beer array
    
    * create initial style file for beerCard
    
    * add styles for beer card
    
    * create BeerImage and BeerCardContent for BeerCard component
    
    * imrove BeerCard styles
    
    * fix styles in BeerCard component
    
    * fix beer card styles
    
    * imort Link in app.jsx

--- src/containers/landingPage/LandingPage.jsx
+++ src/containers/landingPage/LandingPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import {Layout} from "../layout/Layout.jsx";
+import {BeerList} from "../beerList/BeerList.jsx";
+import './LandingPage.scss'
 
 const LandingPage = () => ( 
     <Layout>
-      <div className="landing-page">landing!</div>
+      <div className="landing-page">
+        landing!
+        <BeerList/>
+      </div> 
     </Layout> 
+
 );
 
 export default LandingPage;
