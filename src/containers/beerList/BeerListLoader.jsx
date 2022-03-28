e1e8adf0d88609f0ea5b3366fd9338936dc93f64
Cyber Slug <lasminski@mail.ru>
2022-03-28 12:50:06 +0300

    add error component to beerLoader

--- src/containers/beerList/BeerListLoader.jsx
+++ src/containers/beerList/BeerListLoader.jsx
@@ -1,22 +1,28 @@
 import React,{ useEffect, useState } from "react";
+import ErrorMessage from "../../components/error/ErrorMessage";
 import { fetchBeers } from "../../utils/api";
 import { LoadingSpinner } from "../loadingSpinner/LoadingSpinner.jsx";
 import { BeerList } from './BeerList.jsx'
 
 export const BeerListLoader = () => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [beersArray, setBeersArray] = useState([]);
+    const [requestStatus, setRequestStatus] = useState({isLoading: true, isError: false})
+    const [beers, setBeers] = useState([]);
     useEffect(
         async () => {
-            const beersList = await fetchBeers()
-            setBeersArray(beersList)
-            setIsLoading(false)
+            try {
+                const beersList = await fetchBeers()
+                setBeers(beersList)
+                setRequestStatus({isLoading: true, isError: false})
+            } catch (e) {
+                setRequestStatus({isLoading: false, isError: true})
+            }        
         },[])
 
     return (
-        <div>
-            <BeerList beersArray={beersArray}/>
-            {isLoading && <LoadingSpinner className="beer-list-loader__loading-spinner"/>}
+        <div className="beer-list-loader">
+            <BeerList beersArray={beers}/>
+            {requestStatus.isError && <ErrorMessage/>}
+            {requestStatus.isLoading && <LoadingSpinner className="beer-list-loader__loading-spinner"/>}
         </div>
     )
 }
