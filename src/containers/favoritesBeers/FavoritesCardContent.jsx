c626f1039cff97ccbfa9a0072f1795c6490ff2b6
Cyber Slug <lasminski@mail.ru>
2022-03-29 16:43:55 +0300

    fix favorites card styles

--- src/containers/favoritesBeers/FavoritesCardContent.jsx
+++ src/containers/favoritesBeers/FavoritesCardContent.jsx
@@ -8,8 +8,10 @@ export const FavoritesCardContent = ({title, tagline, description}) => (
     <h1 className="font-h1 favorites-card-content__title">{title}</h1>
     <h2 className="font-h2-alternative favorites-card-content__tagline">{tagline}</h2>
     <h3 className="font-h2 favorites-card-content__description">{description}</h3>
-    <Button name="OPEN" className="favorites-card-content__open-button" onClick={() => {throw new Error()}}/>
-    <Button name="Favorites" className="favorites-card-content__favorites-button" onClick={() => {throw new Error()}}/>
+    <div className="favorites-card-content__button-panel">
+      <Button name="OPEN" className="favorites-card-content__open-button" onClick={() => {throw new Error()}}/>
+      <Button name="Favorites" className="favorites-card-content__favorites-button" onClick={() => {throw new Error()}}/>
+    </div>
   </div>
   
 );
