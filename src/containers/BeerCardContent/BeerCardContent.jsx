6b725759953e818c829ea0e4251bba806ba4cb92
Cyber Slug <lasminski@mail.ru>
2022-03-29 17:14:13 +0300

    add align items to button panel

--- src/containers/BeerCardContent/BeerCardContent.jsx
+++ src/containers/BeerCardContent/BeerCardContent.jsx
@@ -5,7 +5,7 @@ import './BeerCardContent.scss';
 export const BeerCardContent = ({title, tagline}) => (
     <div className="beer-card-content">
     <h1 className="font-h1 beer-card-content__title">{title}</h1>
-    <h2 className="font-h2 beer-card-content__title">{tagline}</h2>
+    <h2 className="font-h2 beer-card-content__tagline">{tagline}</h2>
     <div className="beer-card-content__button-panel">
     <Button name="OPEN" className="beer-card-content__open-button" onClick={() => {throw new Error()}}/>
     <Button name="Favorites" className="beer-card-content__favorites-button" onClick={() => {throw new Error()}}/>
