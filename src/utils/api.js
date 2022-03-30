8a3988a23f9da153711499a0eb1be633949d34fa
Cyber Slug <lasminski@mail.ru>
2022-03-30 17:26:44 +0300

    move request params into array

--- src/utils/api.js
+++ src/utils/api.js
@@ -13,12 +13,12 @@ const customFetch = async (url, method = "GET") => {
 }
 
 export const fetchBeers = async (page = 1, input = '', alcoholVolume = 0, IBU = 0, EBC = 0) => {
-    const nameParam = input.text ? `beer_name=${input.text}&` : '';
-    const alcParam = `abv_gt=${alcoholVolume}&`;
-    const bitternessParam = `ibu_gt=${IBU}&`;
-    const ebcColorParam = `ebc_gt=${EBC}&`;
+    const nameParam = input.text ? `beer_name=${input.text}` : '';
+    const alcParam = `abv_gt=${alcoholVolume}`;
+    const bitternessParam = `ibu_gt=${IBU}`;
+    const ebcColorParam = `ebc_gt=${EBC}`;
     const pageParam = `page=${page}`
-    const url = `${defaultUrl}/beers?${nameParam}${alcParam}${bitternessParam}${ebcColorParam}${pageParam}`;
+    const url = `${defaultUrl}/beers?${[nameParam, alcParam, bitternessParam, ebcColorParam, pageParam].filter(e => e!=='').join('&')}`;
     const response = await customFetch(url);
     return response;     
 }
