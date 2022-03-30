57e9521ba21aa7f28c488324ff1d511d0365b442
Cyber Slug <lasminski@mail.ru>
2022-03-30 15:32:20 +0300

    fix side meny position styles

--- src/containers/sideMenu/SideMenuBlock.jsx
+++ src/containers/sideMenu/SideMenuBlock.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { SideMenu } from "./SideMenu.jsx";
+import './SideMenuBlock.scss';
 
 export const SideMenuBlock = ({onMenuButtonClick, className}) => (
 
-    <div onClick={onMenuButtonClick} className={`${className}-block`}>
+    <div onClick={onMenuButtonClick} className="side-menu-block">
         <SideMenu className={className}/>
     </div>
 )
