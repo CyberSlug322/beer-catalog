e6f144e383f0441eabd0d6c2d583d2a674eef6f2
Cyber Slug <lasminski@mail.ru>
2022-03-31 00:35:39 +0300

    fix infinit requests issue

--- src/containers/beerList/BeerListLoader.jsx
+++ src/containers/beerList/BeerListLoader.jsx
@@ -5,49 +5,46 @@ import { LoadingSpinner } from "../loadingSpinner/LoadingSpinner.jsx";
 import { BeerList } from './BeerList.jsx'
 
 export const BeerListLoader = ({input, alcoholVolume, IBU, EBC}) => {
-    const [requestStatus, setRequestStatus] = useState({isLoading: true, isError: false})
+    const [requestStatus, setRequestStatus] = useState({isLoading: true, isError: false, isAllBeersLoaded: false})
     const [beers, setBeers] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    console.log("render")
-    const handleScroll = () => {
-        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
-        
-        setCurrentPage(currentPage + 1)
 
+    const handleScroll = () => {
+        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;  
+        setCurrentPage(currentPage + 1)
       }
 
     useEffect(
         () => {
-        window.addEventListener('scroll', handleScroll);
+        !requestStatus.isAllBeersLoaded && window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
-      }, []);
+      }, [currentPage]);
 
     useEffect(
         async () => {
             try {
-                setRequestStatus({isLoading: true, isError: false})
+                setRequestStatus({...requestStatus, isLoading: true, isError: false})
                 setCurrentPage(1)
                 const beersList = await fetchBeers(currentPage, input)
                 setBeers(beersList)
-                setRequestStatus({isLoading: false, isError: false})
+                setRequestStatus({...requestStatus, isLoading: false, isError: false})
             } catch (e) {
                 console.log(e)
-                setRequestStatus({isLoading: false, isError: true})
+                setRequestStatus({...requestStatus, isLoading: false, isError: true})
             }
         },[input]) 
 
     useEffect(
         async () => {
-            console.log(currentPage,"c page")
             if (currentPage === 1) return;
             try {
-                setRequestStatus({isLoading: true, isError: false})
+                setRequestStatus({...requestStatus, isLoading: true, isError: false})
                 const beersList = await fetchBeers(currentPage, input)
                 setBeers([...beers, ...beersList])
-                setRequestStatus({isLoading: false, isError: false})
+                setRequestStatus({...requestStatus, isLoading: false, isError: false, isAllBeersLoaded: beersList.length === 0})
             } catch (e) {
                 console.log(e)
-                setRequestStatus({isLoading: false, isError: true})
+                setRequestStatus({...requestStatus, isLoading: false, isError: true})
             }
         },[currentPage])
     
