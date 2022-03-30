c2f2974586d7e2fd479fe5c77e73b52e9efbe674
Cyber Slug <lasminski@mail.ru>
2022-03-30 17:50:58 +0300

    add infinite scroll

--- src/containers/beerList/BeerListLoader.jsx
+++ src/containers/beerList/BeerListLoader.jsx
@@ -7,12 +7,27 @@ import { BeerList } from './BeerList.jsx'
 export const BeerListLoader = ({input, alcoholVolume, IBU, EBC}) => {
     const [requestStatus, setRequestStatus] = useState({isLoading: true, isError: false})
     const [beers, setBeers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    console.log("render")
+    const handleScroll = () => {
+        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+        
+        setCurrentPage(currentPage + 1)
+
+      }
+
+    useEffect(
+        () => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+      }, []);
 
     useEffect(
         async () => {
             try {
                 setRequestStatus({isLoading: true, isError: false})
-                const beersList = await fetchBeers(1, input)
+                setCurrentPage(1)
+                const beersList = await fetchBeers(currentPage, input)
                 setBeers(beersList)
                 setRequestStatus({isLoading: false, isError: false})
             } catch (e) {
@@ -20,6 +35,23 @@ export const BeerListLoader = ({input, alcoholVolume, IBU, EBC}) => {
                 setRequestStatus({isLoading: false, isError: true})
             }
         },[input]) 
+
+    useEffect(
+        async () => {
+            console.log(currentPage,"c page")
+            if (currentPage === 1) return;
+            try {
+                setRequestStatus({isLoading: true, isError: false})
+                const beersList = await fetchBeers(currentPage, input)
+                setBeers([...beers, ...beersList])
+                setRequestStatus({isLoading: false, isError: false})
+            } catch (e) {
+                console.log(e)
+                setRequestStatus({isLoading: false, isError: true})
+            }
+        },[currentPage])
+    
+
 
     return (
         <div className="beer-list-loader">
