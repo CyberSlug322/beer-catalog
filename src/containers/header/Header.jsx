e23db2d85da39b22e761b28b13452f7d867ac564
CyberSlug322 <58426879+CyberSlug322@users.noreply.github.com>
2022-03-31 19:08:18 +0300

    Add side menu component (#21)
    * create SideMenuBlock component
    
    * add stopPropagation to onClick event in side menu
    
    * fix side meny position styles
    
    * fix MenuButton, add onClick handler
    
    * add useRef hook to sideMenuBlock
    
    * fix positioning styles in side menu block

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,11 +2,9 @@ import './Header.scss'
 import React from "react";
 import {MenuButton} from '../../components/button/MenuButton.jsx';
 import './Header.scss'
-import { SideMenu } from '../sideMenu/SideMenu.jsx';
-export const Header = ({className}) => (
+export const Header = ({className, onMenuButtonClick}) => (
     <header className={`header ${className} `}>
-      <MenuButton/>
-      <SideMenu className="header__side-menu"/>
+      <MenuButton onMenuButtonClick={onMenuButtonClick}/>
       <label className='header__title font-h1'>Beer Catalog </label>
     </header>
 );
